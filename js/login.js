bd8cea683bbe0fb10c504fb9881c9cda4ea5e688
dasyerman <germanortigoza99@gmail.com>
2022-08-15 19:21:58 -0300

    Login v2

---
+++ js/login.js
@@ -0,0 +1,9 @@
+//My code
+
+document.addEventListener("DOMContentLoaded", function(){
+document.getElementById("login-button").addEventListener("click", function() {
+    localStorage.setItem("catID", 103);
+    window.location = "index.html"
+});
+
+})
